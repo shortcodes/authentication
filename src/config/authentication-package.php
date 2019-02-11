25ab7de841587864b52be37d69b69fe758d3a6c0
roman.szymanski <r.szymanski@mindz.it>
2019-02-11 11:24:03 +0100

    Fix prefixes

--- src/config/authentication-package.php
+++ src/config/authentication-package.php
@@ -1,11 +1,15 @@
 <?php
 
 return [
+    'prefix' => [
+        'v1'
+    ],
     'routes' => [
         'login' => 'login',
         'register' => 'register',
         'confirm-registration' => '/register/{token}',
         'remind-password' => '/remind-password',
         'reset-password' => '/reset-password',
+        'change-password' => '/account/change-password',
     ],
 ];
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -4,8 +4,16 @@ use Shortcodes\Authentication\Controllers\Auth\LoginController;
 use Shortcodes\Authentication\Controllers\Auth\PasswordController;
 use Shortcodes\Authentication\Controllers\Auth\RegisterController;
 
-Route::post(config('authentication-package.routes.login', '/login'), [LoginController::class, 'login'])->name('login');
-Route::post(config('authentication-package.routes.register', '/register'), [RegisterController::class, 'register'])->name('register');
-Route::post(config('authentication-package.routes.confirm-registration', '/register/{token}'), [RegisterController::class, 'confirmRegistration'])->name('confirm-registration');
-Route::post(config('authentication-package.routes.remind-password', '/remind-password'), [PasswordController::class, 'remindPassword'])->name('remind-password');
-Route::post(config('authentication-package.routes.reset-password', '/reset-password'), [PasswordController::class, 'resetPassword'])->name('reset-password');
+Route::group(['prefix' => config('authentication-package.prefix', 'v1')], function () {
+
+    Route::post(config('authentication-package.routes.login', '/login'), [LoginController::class, 'login'])->name('login');
+    Route::post(config('authentication-package.routes.register', '/register'), [RegisterController::class, 'register'])->name('register');
+    Route::post(config('authentication-package.routes.confirm-registration', '/register/{token}'), [RegisterController::class, 'confirmRegistration'])->name('confirm-registration');
+    Route::post(config('authentication-package.routes.remind-password', '/remind-password'), [PasswordController::class, 'remindPassword'])->name('remind-password');
+    Route::post(config('authentication-package.routes.reset-password', '/reset-password'), [PasswordController::class, 'resetPassword'])->name('reset-password');
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post(config('authentication-package.routes.change-password', '/account/change-password'), [PasswordController::class, 'changePassword']);
+    });
+
+});
